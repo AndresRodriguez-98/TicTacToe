28335f92efcd616196e6144af90fec3a88a19199
ReloJunior <andresrodriguezab@hotmail.com>
2023-08-16 17:12:11 -0300

    Pasando la carpeta tic tac toe a otro repo

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// PUNTO DE ENTRADA A NUESTRA APP (DESDE DONDE SE CONSTRUYE)
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
